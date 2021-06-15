e82e9c0bf60108cdd21270eaa739f37937dc2843
Ajay Ram K <ajayram192@gmail.com>
2021-06-15 11:42:23 +0530

    botfile-procfile-edit

--- bot.py
+++ bot.py
@@ -9,21 +9,6 @@ from html import escape
 bot = telebot.TeleBot(token)
 
 
-# Server deploy
-@server.route('/' + token, methods=['POST'])
-def getMessage():
-   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-   return "!", 200@server.route("/")
-def webhook():
-   bot.remove_webhook()
-   bot.set_webhook(url='https://sethurama-iyer-bot.herokuapp.com' + token)
-   return "!", 200if __name__ == "__main__":
-   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-
-
-
-
-
 def getImg(photo_info, message):
 	
 	img_url = "https://api.telegram.org/file/bot%s/%s" % (token, photo_info.file_path)
