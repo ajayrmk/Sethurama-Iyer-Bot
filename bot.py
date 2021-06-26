be038aa65d381d300aaab068832cc6a568072e93
Ajay Ram K <ajayram192@gmail.com>
2021-06-27 01:39:18 +0530

    new py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 # Import Libraries
 import telebot
-from telebot import types
 from config import token
 import wikipedia
 import pickle
@@ -16,24 +15,25 @@ model = pickle.load(open('model.pkl','rb'))
 
 # Wikiscrap and predict function 
 def wikiData(response):
-    try:
-        page = wikipedia.page(response)
-        data = page.summary
-    except:
-        topics = wikipedia.search(response)
-        data = wikipedia.summary(topics[1])
-    return model.predict(vectorizer.transform([data]))[0]
+	try:
+		page = wikipedia.page(response)
+		data = page.summary
+	except:
+		topics = wikipedia.search(response)
+		data = wikipedia.summary(topics[1])
+
+	return model.predict(vectorizer.transform([data]))[0]
 
 # Bot functions
 @bot.message_handler(content_types=['text'])
 def welcome(pm):
     sent_msg = bot.send_message(pm.chat.id, "Welcome to CBI. Send in the name of the first accused.")
-    bot.register_next_step_handler(sent_msg, first_handler) #Next message will call the name_handler function
+    bot.register_next_step_handler(sent_msg, first_handler) #Next message will call the first_handler function
     
 def first_handler(pm):
     first = pm.text
     sent_msg = bot.send_message(pm.chat.id, f"First accused is {first}. Send in the name of the second accused.")
-    bot.register_next_step_handler(sent_msg, second_handler, first) #Next message will call the age_handler function
+    bot.register_next_step_handler(sent_msg, second_handler, first) #Next message will call the second_handler function
 
 def second_handler(pm, first):
 	second = pm.text
