d7b23c34b3f8c356538c420fe9c4569c27ee05e2
Ajay Ram K <ajayram192@gmail.com>
2021-06-27 01:02:04 +0530

    new py

--- bot.py
+++ bot.py
@@ -1,22 +1,48 @@
+# Import Libraries
 import telebot
 from telebot import types
 from config import token
+import pandas as pd
+import numpy as np
+import wikipedia
+import pickle
 
 # Telebot
 bot = telebot.TeleBot(token)
 
+# Open and load SVM models
+vectorizer = pickle.load(open('vectorizer_model.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+vectorizer = pickle.load(open('vectorizer_model.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+# Wikiscrap and predict function 
+def wikiDataCat(response):
+    try:
+        page = wikipedia.page(response)
+        data = [page.summary]
+    except:
+        topics = wikipedia.search(response)
+        data = [wikipedia.summary(topics[1])]
+
+    return model.predict(vectorizer.transform(data))[0]
+
+# Bot functions
 @bot.message_handler(content_types=['text'])
 def welcome(pm):
-    sent_msg = bot.send_message(pm.chat.id, "Welcome to bot. what's your name?")
-    bot.register_next_step_handler(sent_msg, name_handler) #Next message will call the name_handler function
+    sent_msg = bot.send_message(pm.chat.id, "Welcome to CBI. Send in the name of the first accused.")
+    bot.register_next_step_handler(sent_msg, first_handler) #Next message will call the name_handler function
     
-def name_handler(pm):
-    name = pm.text
-    sent_msg = bot.send_message(pm.chat.id, f"Your name is {name}. how old are you?")
-    bot.register_next_step_handler(sent_msg, age_handler, name) #Next message will call the age_handler function
+def first_handler(pm):
+    first = pm.text
+    sent_msg = bot.send_message(pm.chat.id, f"First accused is {first}. Send in the name of the second accused.")
+    bot.register_next_step_handler(sent_msg, second_handler, first) #Next message will call the age_handler function
 
-def age_handler(pm, name):
-    age = pm.text
-    bot.send_message(pm.chat.id, f"Your name is {name}, and your age is {age}.")
-
+def second_handler(pm, first):
+	second = pm.text
+	if wikiDataCat(first) == wikiDataCat(second):
+		bot.send_message(pm.chat.id, f"{first} and {second} are related. Both are from the field of {wikiDataCat(first)}.")
+	else:
+		bot.send_message(pm.chat.id, f"They are not related.")
+		
 bot.polling()
